bc16a5810057bca6020b2c8445d8ffdb40bcc673
aditbaks <andmur007@gmail.com>
2024-04-10 16:10:28 -0400

    github link add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,14 @@ import "./Footer.css";
 
 function Footer() {
 	return (
-		<footer id="footer" className="footer">
-			<p>
-				Made by Aditya Bakshi
-				<br />
-			</p>
-		</footer>
+	  <footer id="footer" className="footer" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px' }}>
+		<a href="https://github.com/aditbaks/ElectronicShop" target="_blank" rel="noopener noreferrer">
+		  Made by Aditya Bakshi
+		</a>
+	  </footer>
 	);
-}
+  }
+  
+  
 
 export default Footer;
